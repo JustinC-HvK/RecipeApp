29972eee9b7bf176e3ff42de8ce7de4555ab64e4
JustinC-HvK <JustinC-HvK@users.noreply.github.com>
2021-11-30 08:15:36 +1300

    Adding App

---
+++ RecipeSaveApp/Notes/Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
